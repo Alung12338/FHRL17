f24b2b4add24e31a279ee41dd6f3f3258a275e60
Alung12338 <64249260+Alung12338@users.noreply.github.com>
2020-04-27 07:38:53 +0000

    Add files via upload

---
+++ FHRL.py
@@ -0,0 +1,63 @@
+# -- coding: utf-8 --
+import os
+import sys
+import time
+
+os.system('clear')
+os.system('cowsay Selamat menunaikan ibadah puasa')
+time.sleep(3)
+os.system('toilet -f standard TOOLS')
+time.sleep(2)
+print "\33[31;1m《》《》《》《》《》《》《》《》《》《》《》"
+print "\33[31;1m  Author : Alung"
+print "\33[31;1m  Youtube: Alung official"
+print "\33[37;1m  Wa     : 081391460253"
+print "\33[37;1m  Github : https://github.com/Alung12338"
+print "\33[37;1m《》《》《》《》《》《》《》《》《》《》《》"
+print 
+print "\33[33;1m[♡] Menu pilihan [♡]"
+print "\33[33;1m[1] Phising-Fb"
+print "\33[33;1m[2] Cek ip"
+print "\33[33;1m[3] jadwal buka puasa"
+print "\33[33;1m[4] TOOLS TUANBADUT"
+print "\33[33;1m[5] Ubah kuota ruangguru"
+print "\33[33;1m[00] kembali"
+pilih = raw_input('[?] pilih lur : ')
+if pilih == "1":
+        os.system('git clone https://github.com/Alung12338/Phising-Fb')
+        os.system('cd Phising-Fb')
+        os.system('ls')
+        os.system('python2 Alung.py')
+elif pilih == "2":
+        os.system('ifconfig')
+elif pilih == "3":
+        print "(1)DKI Jakarta 17:52 WIB"
+        print "(2)Bandung 17:47 WIB"
+        print "(3)Semarang 17:32 WIB"
+        print "(4)Yogyakarta 17:36 WIB"
+        print "(5)Surabaya 17:27 WIB"
+        print "(6)Makassar 18:02 WITA"
+        print "(7)Manado 17:46 WITA"
+        print "(8)Pontianak 17:47 WIB"
+        print "(9)Banda Aceh 18:48 WIB"
+        print "(10)Medan 18:33 WIB"
+        print "(11)Padang 18:22 WIB"
+        print "(12)Palembang 18:02 WIB"
+        print "(13)Pekanbaru 18:19 WIB"
+        print "(14)Mataram 18:12 WITA"
+        print "(15)Denpasar 18:15 WITA"
+        print "(16)Palu 18:04 WITA"
+        print "(17)Ambon 18:28 WIT"
+        print "(18)Jayapura 17:39 WIT"
+elif pilih == "4":
+        os.system('git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3')
+        os.system('chmod +x TUANB4DUT.sh')
+        os.system('ls')
+        os.system('sh TUANB4DUT.sh')
+elif pilih == "5":
+        os.system('https://github.com/Alung12338/IlmuNew')
+        os.system('cd IlmuNew')
+        os.system('ls')
+        os.system('python2 main.py')
+elif pilih == "00":
+        os.system('exit')
